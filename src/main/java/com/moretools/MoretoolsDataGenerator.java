686e33b8b18709d37789c84e493ec14f45cf7606
V-Karch <luna.karch05@gmail.com>
2024-08-29 16:27:52 -0400

    MoreTools: a minecraft mod to add a bunch of tools to the game (and teach me how to mod minecraft)

---
+++ src/main/java/com/moretools/MoretoolsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.moretools;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoretoolsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
